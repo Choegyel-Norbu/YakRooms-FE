a0b7aea2095326828fe797097de97b6c5dd70d61
Choegyel <mac@Chogyal.local>
2025-07-20 08:19:06 +0600

    added config railway url

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = "http://localhost:8080/api";
+
+
+
+// const API_BASE_URL = "http://localhost:8080/api";
 // const API_BASE_URL = "https://672f-119-2-125-165.ngrok-free.app";
-// const API_BASE_URL = "https://sdp-platform-production.up.railway.app";
+const API_BASE_URL = "https://yakrooms-be-production.up.railway.app";
 export default API_BASE_URL;
